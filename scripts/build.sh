da4973eb1d0e842ad26d425463a9d280ec452b2d
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-08-23 00:17:01 -0300

    chore: updated buid script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=0
 
-go build -ldflags="-linkmode=internal -w -s -extldflags '-static' -X main.BuildCPUFlags=native" ../cmd/capitech/main.go
+go build -ldflags="-linkmode=internal -w -s -extldflags '-static' -X main.BuildCPUFlags=native" -o capitech_api ../cmd/capitech/main.go
 
-#./updateProd.sh
+./updateProd.sh
