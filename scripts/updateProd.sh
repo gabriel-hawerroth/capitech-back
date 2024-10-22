cbcdb2bceff40d5a5ee6302df808c61ca27ae3ae
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-22 13:26:35 -0300

    chore: use ssh alias

--- scripts/updateProd.sh
+++ scripts/updateProd.sh
@@ -1,8 +1,8 @@
-ssh ubuntu@168.138.150.229 "pm2 delete api.capitech"
-ssh ubuntu@168.138.150.229 "rm -rf /home/ubuntu/prd_projects/back/capitech/capitech_api"
+ssh ubuntu@hawetec "pm2 delete api.capitech"
+ssh ubuntu@hawetec "rm -rf /home/ubuntu/prd_projects/back/capitech/capitech_api"
 
-scp -r capitech_api ubuntu@168.138.150.229:/home/ubuntu/prd_projects/back/capitech/
+scp -r capitech_api ubuntu@hawetec:/home/ubuntu/prd_projects/back/capitech/
 
-ssh ubuntu@168.138.150.229 "/home/ubuntu/start_scripts/start_capitech_api.sh"
+ssh ubuntu@hawetec "/home/ubuntu/start_scripts/start_capitech_api.sh"
 
 rm capitech_api
