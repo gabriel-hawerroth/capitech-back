1b6e027c8ed7f2668c522413d552b149c79ebefc
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-26 15:32:36 -0300

    ci: remove updateProd scripts

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export GOOS=linux
-export GOARCH=amd64
-export CGO_ENABLED=0
-
-go build -ldflags="-linkmode=internal -w -s -extldflags '-static' -X main.BuildCPUFlags=native" -o capitech_api ../cmd/capitech/main.go
-
-./updateProd.sh
--- scripts/updateProd.sh
+++
@@ -1,7 +0,0 @@
-ssh ubuntu@hawetec "pm2 delete api.capitech"
-
-scp -r capitech_api ubuntu@hawetec:/home/ubuntu/prd_projects/back/capitech/
-
-ssh ubuntu@hawetec "/home/ubuntu/start_scripts/start_capitech_api.sh"
-
-rm capitech_api
